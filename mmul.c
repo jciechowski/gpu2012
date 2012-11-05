0b9435c61134a39e44ca0d8e09005592961e0ab8
Jakub Ciechowski <sianex@gmail.com>
2012-11-05 08:47:13 +0100

    mnozenie na cpu oraz pomiar czasu

--- mmul.c
+++ mmul.c
@@ -10,20 +10,28 @@ void genMat(int *a, int m, int n) {
       a[i*j+j] = rand()%10;
 }
 
-void mulMat(int *a, int *b, int *c, int m, int n, int p, int r) {
-  int i, j, k;
+void printMat(int *a, int m, int n) {
+  int i,j;
   for(i=0;i<m;i++) {
-    for(k=0;k<m*r;k++) {
-      for(j=0;j<r;j++) {
-        c[k] += a[n*i+i] * b[j*p+j];
-        printf("%d * %d = %d\n", a[n*i+i], b[j*p+j], c[k]);
-      }
+    for(j=0;j<n;j++)
+      printf("%d%c",a[i*m+j],(j == (m-1))?'\n':'\t');
+  }
+  printf("\n");
+}
+void mulMat(int *a, int *b, int *c, int m, int n, int p, int r) {
+  int i, j, k, sum;
+  for(i=0;i<n;i++) {
+    for(j=0;j<p;j++) {
+      sum = 0;
+      for(k=0;k<m;k++)
+        sum += a[i*m+k]*b[k*p+j];
+      c[i*p+j] = sum;
     }
   }
 }
 
 int main (int argc, char** argv) {
-  int m, n, p, r, i = 0, j;
+  int m, n, p, r, i = 0;
   int *a, *b, *c;
   while( i < argc ){
     m = atoi(argv[++i]);
@@ -45,35 +53,22 @@ int main (int argc, char** argv) {
   genMat(a,m,n);
   genMat(b,p,r);
 
-  if(m < 4 && r < 4) {
-    for(i=0;i<m;i++) {
-      for(j=0;j<n;j++)
-        printf("%d ",a[i*j+i]);
-      printf("\n");
-    }
+  printMat(a,m,n);
+  printMat(b,p,r);
 
-    printf("\n\n");
-    for(i=0;i<p;i++) {
-      for(j=0;j<r;j++)
-        printf("%d ",b[i*j+i]);
-      printf("\n");
-    }
-  }
-  printf("\n");
-
-  
-
+  pTimer zegar = newTimer(); // make new timer
+  startTimer(zegar); // store first timestamp
   mulMat(a, b, c, m, n, p, r);
+  stopTimer(zegar);  // store second timestamp
+  printf("czas mnozenia na CPU: \n"); // show time difference
+  printTimer(zegar);
+  printf("\n\n");
 
-  for(i=0;i<m;i++) {
-    for(j=0;j<n;j++)
-      printf("%d ",c[i*j+j]);
-    printf("\n");
-  }
-  
+  printMat(c,m,r);
+
   free(a);
   free(b);
   free(c);
-
+  freeTimer(zegar);  // free occupied ram
   return 0;
 }
