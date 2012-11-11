0232f9d870ee0bf55f73566d29d543d3711d7ca4
Jakub Ciechowski <sianex@gmail.com>
2012-11-11 13:11:08 +0100

    mnozenie macierzy powyzej 256 elementow

--- cudaMul.cu
+++ cudaMul.cu
@@ -7,8 +7,24 @@ Jakub Ciechowski GPU 2012
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <cuda_runtime.h>
 
+int TILE_WIDTH = 5;
+
+__global__ void tilingMul(int *M, int *N, int *P, int width, int TILE_WIDTH) {
+  
+  int row = blockIdx.y * TILE_WIDTH + threadIdx.y;
+  int col = blockIdx.x * TILE_WIDTH + threadIdx.x;
+  
+  int sum = 0;
+  
+  for(int k = 0; k < width; k++)
+    sum += M[row*width+k] * N[k*width+col];
+  
+  P[row*width+col] = sum;
+  
+}
 
 __global__ void naiveMul(int* M, int* N, int* P, int width) {
 
@@ -30,7 +46,6 @@ void printMat(int *a, int width) {
     printf("\n");
 }
 
-
 int* genMatrix(int width) {
     int *a = (int*)calloc(width*width, sizeof(int));
     int i,j;
@@ -40,7 +55,6 @@ int* genMatrix(int width) {
     return a;
 }
 
-
 int *matMul(int *hostA, int *hostB, int width) {
     int SIZE = width*width;
     int *hostC = (int*)calloc(SIZE,sizeof(int));
@@ -49,14 +63,22 @@ int *matMul(int *hostA, int *hostB, int width) {
     cudaMalloc((void**) &devA, SIZE*sizeof(int));
     cudaMalloc((void**) &devB, SIZE*sizeof(int));
     cudaMalloc((void**) &devC, SIZE*sizeof(int));
+    
+    // do 256 elementow, naiveMul
+//    dim3 blockDim(width, width);
+//    dim3 gridDim(1,1);
 
-    dim3 blockDim(width, width);
-    dim3 gridDim(1,1);
-
+    // powyzej 256
+    TILE_WIDTH = sqrt(width);
+    dim3 blockDim(width/TILE_WIDTH, width/TILE_WIDTH);
+    dim3 gridDim(TILE_WIDTH, TILE_WIDTH);
     cudaMemcpy(devA, hostA, SIZE*sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(devB, hostB, SIZE*sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(devC, hostC, SIZE*sizeof(int), cudaMemcpyHostToDevice);
-    naiveMul<<<gridDim,blockDim>>>(devA, devB, devC, width);
+    // do 256 elementow
+//    naiveMul<<<gridDim,blockDim>>>(devA, devB, devC, width);
+    // powyzej 256
+    tilingMul<<<gridDim, blockDim>>> (devA, devB, devC, width, TILE_WIDTH);
     cudaMemcpy(hostC, devC, SIZE*sizeof(int), cudaMemcpyDeviceToHost);
 
     return hostC;
